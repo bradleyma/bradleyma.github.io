995e3a65b9ebb5273e8f7319f7b0e3dff48ce129
bradleyyma <bradley.y.ma@gmail.com>
2019-12-23 12:10:12 -0700

    Added projects and scroll to top button

---
+++ resources/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#myBtn').fadeIn();
+    } else {
+      $('#myBtn').fadeOut();
+    }
+  });
+
+  $('#myBtn').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 100);
+      return false;
+  });
+
+});
---
+++ resources/js/project_info.js
@@ -0,0 +1,24 @@
+var projects = {
+  'squareup': {
+    'name': 'SquareUp',
+    'date': 'Fall 2019',
+    'details': "Project along with five other peers utilizing Django's Web Framework to develop a web application that connected users with similar outdoor activity interests. It allowed users to create and sign up for events. I gained experience in coding in python, as well as the Git pull request-review-branch system work flow.",
+    'git': 'https://github.com/bradleyyma/Purple-Nut-Project',
+    'link': '',
+  },
+  'tictactoe':{
+    'name': 'TicTacToe',
+    'date': 'Fall 2019',
+    'details': 'Used JavaScript to write a simple Web Based game of Tic Tac Toe. This is currently an ongoing project for improvement.',
+    'git': '',
+    'link': '',
+  },
+  'weatherapi':{
+    'name': 'Weather API',
+    'date': 'Fall 2019',
+    'details': 'Utilized Dark Star API to display the average daily temperature in each state with a respective solid color',
+    'git': '',
+    'link': '',
+  }
+
+}
