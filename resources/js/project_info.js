7bf3759dc3c0c3e48bba14949d48c4c569e62327
Bradley <bradley.y.ma@gmail.com>
2020-01-01 17:34:48 -0700

    snake project, resume added

--- resources/js/project_info.js
+++ resources/js/project_info.js
@@ -1,24 +1,31 @@
 var projects = {
   'squareup': {
-    'name': 'SquareUp',
-    'date': 'Fall 2019',
-    'details': "Project along with five other peers utilizing Django's Web Framework to develop a web application that connected users with similar outdoor activity interests. It allowed users to create and sign up for events. I gained experience in coding in python, as well as the Git pull request-review-branch system work flow.",
-    'git': 'https://github.com/bradleyyma/Purple-Nut-Project',
-    'link': '',
+      'name': 'SquareUp',
+      'date': 'Fall 2019',
+      'details': "Project along with five other peers utilizing Django's Web Framework to develop a web application that connected users with similar outdoor activity interests. It allowed users to create and sign up for events. I gained experience in coding in python, as well as the Git pull request-review-branch system work flow.",
+      'git': 'https://github.com/bradleyyma/Purple-Nut-Project',
+      'link': '',
+  },
+  'snake':{
+      'name': 'Snake',
+      'date': 'Spring 2020',
+      'details': "Utilized Pygame to recreate the simple game of snake in Python. This project was very interesting as a strong introduction to Python and a good refresher in OOP",
+      'git': 'https://github.com/bradleyyma/snake',
+      'link': '',
   },
   'tictactoe':{
-    'name': 'TicTacToe',
-    'date': 'Fall 2019',
-    'details': 'Used JavaScript to write a simple two-player Web Based game of Tic Tac Toe. This is currently an ongoing project for improvement.',
-    'git': 'https://github.com/bradleyyma/tic-tac-toe',
-    'link': 'tic-tac-toe/tic-tac-toe.html',
+      'name': 'TicTacToe',
+      'date': 'Fall 2019',
+      'details': 'Used JavaScript to write a simple two-player Web Based game of Tic Tac Toe. This is currently an ongoing project for improvement.',
+      'git': 'https://github.com/bradleyyma/tic-tac-toe',
+      'link': 'tic-tac-toe/tic-tac-toe.html',
   },
   'weatherapi':{
-    'name': 'Weather API',
-    'date': 'Fall 2019',
-    'details': 'Utilized Dark Star API to display the current temperature and weather, the next day forcast, and a map visual of the average temperaature for each state in the United States. Utilized javascript and css to style and create responsive colors for respective temperatures',
-    'git': 'https://github.com/bradleyyma/weatherapi',
-    'link': 'weatherapi/index.html',
+      'name': 'Weather API',
+      'date': 'Fall 2019',
+      'details': 'Utilized Dark Star API to display the current temperature and weather, the next day forcast, and a map visual of the average temperaature for each state in the United States. Utilized javascript and css to style and create responsive colors for respective temperatures',
+      'git': 'https://github.com/bradleyyma/weatherapi',
+      'link': 'weatherapi/index.html',
   }
 
 }
