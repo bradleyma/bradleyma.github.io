717a0bc9836749b603581c4305e05de584d84132
Bradley <bradley.y.ma@gmail.com>
2020-03-26 21:04:29 -0600

    sudoku project addition

--- resources/js/project_info.js
+++ resources/js/project_info.js
@@ -1,31 +1,38 @@
 var projects = {
-  'squareup': {
-      'name': 'SquareUp',
-      'date': 'Fall 2019',
-      'details': "Project along with five other peers utilizing Django's Web Framework to develop a web application that connected users with similar outdoor activity interests. It allowed users to create and sign up for events. I gained experience in coding in python, as well as the Git pull request-review-branch system work flow.",
-      'git': 'https://github.com/bradleyyma/Purple-Nut-Project',
-      'link': '',
-  },
-  'snake':{
-      'name': 'Snake',
-      'date': 'Spring 2020',
-      'details': "Utilized Pygame to recreate the simple game of snake in Python. This project was very interesting as a strong introduction to Python and a good refresher in OOP",
-      'git': 'https://github.com/bradleyyma/snake',
-      'link': '',
-  },
-  'tictactoe':{
-      'name': 'TicTacToe',
-      'date': 'Fall 2019',
-      'details': 'Utilized Javascript to recreate the basic game of TicTacToe. Implemented a recursive function to implement the minimax algorithm for TicTacToe. This project was great practice for logical thinking and also working in Javascript, Jquery, HTML. </p> <br> <p> <b>Update 2020-</b> Re-coded this project using ReactJS. This was my introduction to ReactJS and learned the structure and basic rules.',
-      'git': 'https://github.com/bradleyyma/tic-tac-toe',
-      'link': 'tic-tac-toe/tic-tac-toe.html',
-  },
-  'weatherapi':{
-      'name': 'Weather API',
-      'date': 'Fall 2019',
-      'details': 'Utilized Dark Star API to display the current temperature and weather, the next day forcast, and a map visual of the average temperaature for each state in the United States. Utilized javascript and css to style and create responsive colors for respective temperatures',
-      'git': 'https://github.com/bradleyyma/weatherapi',
-      'link': 'weatherapi/index.html',
-  }
+    'sudoku': {
+        'name': 'Sudoku',
+        'date': 'Spring 2020',
+        'details': "Individual. Utilized reactJS to develop a reactive sudoku solver/player. The player has a chance to play a \"random\" board, or the user can input their own puzzle. Implemented a backtracking algorithm in order to solve current board.",
+        'git': 'https://github.com/bradleyyma/sudoku',
+        'link': 'https://bradleyyma.github.io/sudoku/'
+    },
+    'squareup': {
+        'name': 'SquareUp',
+        'date': 'Fall 2019',
+        'details': "Project along with five other peers utilizing Django's Web Framework to develop a web application that connected users with similar outdoor activity interests. It allowed users to create and sign up for events. I gained experience in coding in python, as well as the Git pull request-review-branch system work flow.",
+        'git': 'https://github.com/bradleyyma/Purple-Nut-Project',
+        'link': '',
+    },
+        'snake':{
+        'name': 'Snake',
+        'date': 'Spring 2020',
+        'details': "Utilized Pygame to recreate the simple game of snake in Python. This project was very interesting as a strong introduction to Python and a good refresher in OOP",
+        'git': 'https://github.com/bradleyyma/snake',
+        'link': '',
+    },
+        'tictactoe':{
+        'name': 'TicTacToe',
+        'date': 'Fall 2019',
+        'details': 'Utilized Javascript to recreate the basic game of TicTacToe. Implemented a recursive function to implement the minimax algorithm for TicTacToe. This project was great practice for logical thinking and also working in Javascript, Jquery, HTML. </p> <br> <p> <b>Update 2020-</b> Re-coded this project using ReactJS. This was my introduction to ReactJS and learned the structure and basic rules.',
+        'git': 'https://github.com/bradleyyma/tic-tac-toe',
+        'link': 'tic-tac-toe/',
+    },
+        'weatherapi':{
+        'name': 'Weather API',
+        'date': 'Fall 2019',
+        'details': 'Utilized Dark Star API to display the current temperature and weather, the next day forcast, and a map visual of the average temperaature for each state in the United States. Utilized javascript and css to style and create responsive colors for respective temperatures',
+        'git': 'https://github.com/bradleyyma/weatherapi',
+        'link': 'weatherapi/index.html',
+    }
 
 }
